230d77e2777d891a35dee60e089dd45d8a97b4ae
時原 公平 <kohei.tokihara@90201682n.intra.emercury.co.jp>
2013-04-26 20:24:41 +0900

    activity task done

--- assignments/fundamentals/2nd/ControllerAssignment2/src/jp/mixi/assignment/controller/med/SubActivity.java
+++ assignments/fundamentals/2nd/ControllerAssignment2/src/jp/mixi/assignment/controller/med/SubActivity.java
@@ -8,58 +8,74 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 /**
- * TODO: 課題2
- * 画面回転や、他のアプリ・画面の起動等で、状態遷移が起こると、それ以前の状態で持っていたデータが失われてしまいます。
+ * TODO: 課題2 画面回転や、他のアプリ・画面の起動等で、状態遷移が起こると、それ以前の状態で持っていたデータが失われてしまいます。
  * これを防ぐため、この Activity の中で状態管理をしてください。
+ * 
  * @author keishin.yokomaku
  */
 public class SubActivity extends Activity implements TextWatcher {
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        // Hint: 状態遷移が何も起こっていない場合は、savedInstanceState は null です
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sub);
-    }
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// Hint: 状態遷移が何も起こっていない場合は、savedInstanceState は null です
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_sub);
+	}
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        EditText text = (EditText) findViewById(R.id.Editor);
-        text.addTextChangedListener(this);
-    }
+	@Override
+	protected void onStart() {
+		super.onStart();
+		EditText text = (EditText) findViewById(R.id.Editor);
+		text.addTextChangedListener(this);
+	}
 
-    /**
-     * TODO: 復帰処理はこちらか onCreate
-     */
-    @Override
-    protected void onRestoreInstanceState(Bundle savedInstanceState) {
-        super.onRestoreInstanceState(savedInstanceState);
-    }
+	/**
+	 * TODO: 復帰処理はこちらか onCreate
+	 */
+	@Override
+	protected void onRestoreInstanceState(Bundle savedInstanceState) {
+		super.onRestoreInstanceState(savedInstanceState);
 
-    /**
-     * TODO: 保存処理はこちら
-     */
-    @Override
-    protected void onSaveInstanceState(Bundle outState) {
-        super.onSaveInstanceState(outState);
-    }
+		if (savedInstanceState != null) {
+			String str = savedInstanceState.getString("stored_text");
+			TextView textv = (TextView) findViewById(R.id.SyncedText);
+			textv.setText(str);
 
-    @Override
-    protected void onStop() {
-        super.onStop();
-        EditText text = (EditText) findViewById(R.id.Editor);
-        text.removeTextChangedListener(this);
-    }
+			EditText etext = (EditText) findViewById(R.id.Editor);
+			etext.setText(str);
+		}
+	}
 
-    @Override
-    public void afterTextChanged(Editable s) {}
+	/**
+	 * TODO: 保存処理はこちら
+	 */
+	@Override
+	protected void onSaveInstanceState(Bundle outState) {
+		super.onSaveInstanceState(outState);
 
-    @Override
-    public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+		EditText text = (EditText) findViewById(R.id.Editor);
+		Editable str = text.getText();
+		outState.putString("stored_text", str.toString());
+	}
 
-    @Override
-    public void onTextChanged(CharSequence s, int start, int before, int count) {
-        TextView text = (TextView) findViewById(R.id.SyncedText);
-        text.setText(s);
-    }
+	@Override
+	protected void onStop() {
+		super.onStop();
+		EditText text = (EditText) findViewById(R.id.Editor);
+		text.removeTextChangedListener(this);
+	}
+
+	@Override
+	public void afterTextChanged(Editable s) {
+	}
+
+	@Override
+	public void beforeTextChanged(CharSequence s, int start, int count,
+			int after) {
+	}
+
+	@Override
+	public void onTextChanged(CharSequence s, int start, int before, int count) {
+		TextView text = (TextView) findViewById(R.id.SyncedText);
+		text.setText(s);
+	}
 }
---
+++ assignments/fundamentals/2nd/ControllerAssignment3/src/jp/mixi/assignment/controller/adv/MainFragment.java
@@ -0,0 +1,23 @@
+package jp.mixi.assignment.controller.adv;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MainFragment extends Fragment {
+	public MainFragment(){}
+	
+    public static Fragment createInstance() {
+        Fragment fragment = new MainFragment();
+        
+        return fragment;
+    }
+    
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+        return view;
+    }
+}
