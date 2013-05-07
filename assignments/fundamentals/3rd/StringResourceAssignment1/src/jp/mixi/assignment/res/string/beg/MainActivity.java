96375b6dc51975a421c6ca0794b70cc55c3c3946
時原 公平 <kohei.tokihara@90201682n.intra.emercury.co.jp>
2013-05-07 17:19:50 +0900

    string assignment

--- assignments/fundamentals/3rd/StringResourceAssignment1/src/jp/mixi/assignment/res/string/beg/MainActivity.java
+++ assignments/fundamentals/3rd/StringResourceAssignment1/src/jp/mixi/assignment/res/string/beg/MainActivity.java
@@ -1,16 +1,25 @@
-
 package jp.mixi.assignment.res.string.beg;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
 
-        // TODO 1 個のものと、複数個のものの両方を並べて表示するため、両方用の TextView を取り出し、リソースへアクセスして表示する
-    }
+		// TODO 1 個のものと、複数個のものの両方を並べて表示するため、両方用の TextView を取り出し、リソースへアクセスして表示する
+		String onequantity = getResources().getQuantityString(R.plurals.my_apple,
+				1,1);		
+		TextView onetext = (TextView)findViewById(R.id.PluralsTextForOne);
+		onetext.setText(onequantity);
+		
+		String otherquantity = getResources().getQuantityString(R.plurals.my_apple,
+				100,100);		
+		TextView othertext = (TextView)findViewById(R.id.PluralsTextForOther);
+		othertext.setText(otherquantity);
+	}
 }
