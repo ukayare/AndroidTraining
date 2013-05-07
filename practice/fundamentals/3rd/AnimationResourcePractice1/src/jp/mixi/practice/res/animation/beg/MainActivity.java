26789155d80cf233640f7a57901d9a07cecb5eab
時原 公平 <kohei.tokihara@90201682n.intra.emercury.co.jp>
2013-05-07 14:08:22 +0900

    drawable and animation

--- practice/fundamentals/3rd/AnimationResourcePractice1/src/jp/mixi/practice/res/animation/beg/MainActivity.java
+++ practice/fundamentals/3rd/AnimationResourcePractice1/src/jp/mixi/practice/res/animation/beg/MainActivity.java
@@ -1,23 +1,33 @@
-
 package jp.mixi.practice.res.animation.beg;
 
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 
 public class MainActivity extends Activity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
+		View textView = findViewById(R.id.text1);
+
+		// Animation Resource を読み込んで、Animation オブジェクトを得る
+		Animation animation = AnimationUtils.loadAnimation(this,
+				R.anim.sample_animation);
+		// Animation オブジェクトを View に渡して、アニメーションを開始する
+		textView.startAnimation(animation);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
 
 }
