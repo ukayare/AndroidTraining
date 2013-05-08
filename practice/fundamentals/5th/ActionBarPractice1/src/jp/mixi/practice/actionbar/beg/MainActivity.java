e506bdc71a93c41bad236dbeb33bf7b1185f4ccb
時原 公平 <kohei.tokihara@90201682n.intra.emercury.co.jp>
2013-05-08 15:53:05 +0900

    actionbar and interaction practice

--- practice/fundamentals/5th/ActionBarPractice1/src/jp/mixi/practice/actionbar/beg/MainActivity.java
+++ practice/fundamentals/5th/ActionBarPractice1/src/jp/mixi/practice/actionbar/beg/MainActivity.java
@@ -1,23 +1,46 @@
-
 package jp.mixi.practice.actionbar.beg;
 
 import android.os.Bundle;
-import android.app.Activity;
-import android.view.Menu;
+import android.support.v4.app.FragmentTransaction;
+import android.widget.Toast;
 
-public class MainActivity extends Activity {
+import com.actionbarsherlock.app.ActionBar;
+import com.actionbarsherlock.app.ActionBar.TabListener;
+import com.actionbarsherlock.app.SherlockActivity;
+import com.actionbarsherlock.app.ActionBar.Tab;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+public class MainActivity extends SherlockActivity implements
+		 TabListener {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		setTheme(R.style.Theme_Sherlock);
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		final ActionBar mActionBar = getSupportActionBar();
+		 
+        mActionBar.addTab(mActionBar.newTab().setText("tab1").setTabListener(this));
+        mActionBar.addTab(mActionBar.newTab().setText("tab2").setTabListener(this));
+        mActionBar.addTab(mActionBar.newTab().setText("tab3").setTabListener(this));
+ 
+        mActionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+ 	}
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
 
+	@Override
+	public void onTabSelected(Tab tab, FragmentTransaction ft) {
+		// TODO Auto-generated method stub
+		
+		Toast.makeText(this, tab.getText(), Toast.LENGTH_LONG).show();
+	}
+	@Override
+	public void onTabUnselected(Tab tab, FragmentTransaction ft) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void onTabReselected(Tab tab, FragmentTransaction ft) {
+		// TODO Auto-generated method stub
+		
+	}
 }
--- practice/fundamentals/5th/InteractionCallbackPractice1/src/jp/mixi/practice/interaction/beg/MainActivity.java
+++ practice/fundamentals/5th/InteractionCallbackPractice1/src/jp/mixi/practice/interaction/beg/MainActivity.java
@@ -1,23 +1,63 @@
-
 package jp.mixi.practice.interaction.beg;
 
 import android.os.Bundle;
-import android.app.Activity;
-import android.view.Menu;
+import android.widget.Toast;
 
-public class MainActivity extends Activity {
+import com.actionbarsherlock.app.ActionBar;
+import com.actionbarsherlock.app.ActionBar.OnNavigationListener;
+import com.actionbarsherlock.app.SherlockActivity;
+import com.actionbarsherlock.view.Menu;
+import com.actionbarsherlock.view.MenuItem;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+public class MainActivity extends SherlockActivity implements
+		OnNavigationListener {
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		getSupportActionBar().setDisplayOptions(0, ActionBar.DISPLAY_SHOW_TITLE);
+	}
 
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getSupportMenuInflater().inflate(R.menu.main, menu);
+
+		menu.add("settings")
+				.setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM);
+		menu.add("login")
+				.setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM);
+		menu.add("refresh")
+				.setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM);
+		return true;
+	}
+
+	@Override
+	public boolean onPrepareOptionsMenu(Menu menu) {
+		// ここで、状態に応じてメニューの有効・無効を切り替えたりなどの処理をする
+		return super.onPrepareOptionsMenu(menu);
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// 選択されたメニューに対応するイベント処理をここで実行する
+		Toast.makeText(this, item.getTitle(), Toast.LENGTH_LONG).show();
+		return super.onOptionsItemSelected(item);
+	}
+
+	// List Navigation の一覧から項目を選択したら呼び出されるコールバック
+	@Override
+	public boolean onNavigationItemSelected(int itemPosition, long itemId) {
+		return false;
+	}
 }
+
+
+/*
+ * レポート、どこに書けばいいかわかんなかったんでここに記載
+ * 上の状態　WXGA720だと縦向きでrefreshが隠れる（横だと出る）QVGAもおなじ
+ * WXGA800だと縦横ともに全部出た
+ * 
+ * */
