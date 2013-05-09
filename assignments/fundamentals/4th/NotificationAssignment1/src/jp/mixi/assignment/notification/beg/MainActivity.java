c50a01af3582594f84a680e5c4ca1cfd2baf0d2d
時原 公平 <kohei.tokihara@90201682n.intra.emercury.co.jp>
2013-05-09 15:44:45 +0900

    notify assignment

--- assignments/fundamentals/4th/NotificationAssignment1/src/jp/mixi/assignment/notification/beg/MainActivity.java
+++ assignments/fundamentals/4th/NotificationAssignment1/src/jp/mixi/assignment/notification/beg/MainActivity.java
@@ -2,7 +2,13 @@
 package jp.mixi.assignment.notification.beg;
 
 import android.app.Activity;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
 
 public class MainActivity extends Activity {
     @Override
@@ -11,5 +17,23 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
 
         // TODO ここで通知を表示する
+        Intent intent = new Intent(this, MainActivity.class);
+		PendingIntent pendingIntent = PendingIntent.getActivity(this, 0,
+				intent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+		NotificationCompat.Builder builder = new NotificationCompat.Builder(
+				this);
+		Notification notification = builder.setWhen(System.currentTimeMillis())
+				.setContentTitle("test").setContentText("tetetest")
+				.setSmallIcon(R.drawable.ic_launcher).setTicker("this is test")
+				.setContentIntent(pendingIntent).setOnlyAlertOnce(true)
+				.setAutoCancel(true).build();
+		
+		notification.vibrate = new long[]{10, 3000};
+
+		NotificationManager manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+		manager.notify(0, notification);
+		
+		
     }
 }
--- assignments/fundamentals/4th/NotificationAssignment2/src/jp/mixi/assignment/notification/med/MainActivity.java
+++ assignments/fundamentals/4th/NotificationAssignment2/src/jp/mixi/assignment/notification/med/MainActivity.java
@@ -2,7 +2,13 @@
 package jp.mixi.assignment.notification.med;
 
 import android.app.Activity;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
 
 public class MainActivity extends Activity {
     public static final String ACITON_VIEW_MY_OWN = "jp.mixi.assignment.notification.med.android.intent.action.VIEW_MY_OWN";
@@ -13,6 +19,21 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
 
         // TODO ここで通知を表示する
+        Intent intent = new Intent(ACITON_VIEW_MY_OWN);
+		PendingIntent pendingIntent = PendingIntent.getActivity(this, 0,
+				intent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+		NotificationCompat.Builder builder = new NotificationCompat.Builder(
+				this);
+		Notification notification = builder.setWhen(System.currentTimeMillis())
+				.setContentTitle("test").setContentText("tetetest")
+				.setSmallIcon(R.drawable.ic_launcher).setTicker("this is test")
+				.setContentIntent(pendingIntent).setOnlyAlertOnce(true)
+				.setAutoCancel(true).build();
+
+		NotificationManager manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+		manager.notify(0, notification);
+		
         // TODO intent には、ACTION_VIEW_MY_OWN の action をセットするだけにしておく
         // TODO 通知をタップした時に、複数の選択肢が表示されるが、どれがどの Activity に対応しているかがわかりづらくなっているので、AndroidManifest を編集して表示をわかりやすくする
     }
