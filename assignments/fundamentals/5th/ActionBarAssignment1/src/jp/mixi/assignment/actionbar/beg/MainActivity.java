5721b34c0cc835cdd0d3a435f56d9ce667c1ff92
tokihara ukayare <kohei.tokihara@90201682n.intra.emercury.co.jp>
2013-05-20 14:42:39 +0900

    actionbae assignment

--- assignments/fundamentals/5th/ActionBarAssignment1/src/jp/mixi/assignment/actionbar/beg/MainActivity.java
+++ assignments/fundamentals/5th/ActionBarAssignment1/src/jp/mixi/assignment/actionbar/beg/MainActivity.java
@@ -3,7 +3,11 @@ package jp.mixi.assignment.actionbar.beg;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
@@ -11,6 +15,15 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        Button button1 = (Button) findViewById(R.id.button1);
+        
+        button1.setOnClickListener(new OnClickListener(){
+        	public void onClick(View v){
+        		Intent i = new Intent(MainActivity.this,NaviActivity.class);
+        		startActivityForResult(i, 0);
+        	}
+        });
     }
 
     @Override
--- assignments/fundamentals/5th/ActionBarAssignment2/src/jp/mixi/assignment/actionbar/med/MainActivity.java
+++ assignments/fundamentals/5th/ActionBarAssignment2/src/jp/mixi/assignment/actionbar/med/MainActivity.java
@@ -1,23 +1,81 @@
 
 package jp.mixi.assignment.actionbar.med;
 
-import android.os.Bundle;
-import android.app.Activity;
-import android.view.Menu;
+import java.util.ArrayList;
+import java.util.List;
 
-public class MainActivity extends Activity {
+import android.os.Bundle;
+import android.support.v4.app.FragmentTransaction;
+import android.widget.ArrayAdapter;
+
+import com.actionbarsherlock.app.ActionBar;
+import com.actionbarsherlock.app.ActionBar.OnNavigationListener;
+import com.actionbarsherlock.app.ActionBar.Tab;
+import com.actionbarsherlock.app.ActionBar.TabListener;
+import com.actionbarsherlock.app.SherlockActivity;
+import com.actionbarsherlock.view.Menu;
+
+public class MainActivity extends SherlockActivity implements TabListener, OnNavigationListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+		
+		final ActionBar mActionBar = getSupportActionBar();
+		 
+        mActionBar.addTab(mActionBar.newTab().setText("tab1").setTabListener(this));
+        mActionBar.addTab(mActionBar.newTab().setText("tab2").setTabListener(this));
+        mActionBar.addTab(mActionBar.newTab().setText("tab3").setTabListener(this));
+             
+        mActionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_LIST);
+        
+		final ActionBar mActionBar2 = getSupportActionBar();
+        
+        List<String> list = new ArrayList<String>();
+        list.add("Navi Menu 1");
+        list.add("Navi Menu 2");
+        list.add("Navi Menu 3");
+        
+        mActionBar2.setListNavigationCallbacks(
+                new ArrayAdapter<String>(getApplicationContext(),
+                        android.R.layout.simple_list_item_1,
+                        android.R.id.text1, list),
+                this);
+
+        getSupportActionBar().setDisplayOptions(0, ActionBar.DISPLAY_SHOW_TITLE);        
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
+    	getSupportMenuInflater().inflate(R.menu.main, menu);
+    	return true;
     }
+
+	@Override
+	public void onTabSelected(Tab tab, FragmentTransaction ft) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onTabUnselected(Tab tab, FragmentTransaction ft) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onTabReselected(Tab tab, FragmentTransaction ft) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean onNavigationItemSelected(int itemPosition, long itemId) {
+		// TODO Auto-generated method stub
+		return false;
+	}
 
 }
