185a99f837c4f0e3a80535bcd4eaf223f2aa3f2d
tokihara ukayare <kohei.tokihara@90201682n.intra.emercury.co.jp>
2013-05-22 15:53:35 +0900

    interaction assignment2

--- assignments/fundamentals/5th/InteractionCallbackAssignment2/src/jp/mixi/assignment/interaction/med/MainActivity.java
+++ assignments/fundamentals/5th/InteractionCallbackAssignment2/src/jp/mixi/assignment/interaction/med/MainActivity.java
@@ -3,14 +3,21 @@ package jp.mixi.assignment.interaction.med;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.Menu;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements TextWatcher {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        EditText editText = (EditText)findViewById(R.id.editText1);
+        editText.addTextChangedListener(this);
     }
 
     @Override
@@ -19,5 +26,28 @@ public class MainActivity extends Activity {
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
+
+	@Override
+	public void afterTextChanged(Editable s) {
+		// TODO Auto-generated method stub
+		TextView tv = (TextView)findViewById(R.id.textView1);
+		
+		tv.setText(s.toString().length()+"");
+		
+	}
+
+	@Override
+	public void beforeTextChanged(CharSequence arg0, int arg1, int arg2,
+			int arg3) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onTextChanged(CharSequence text, int arg1, int arg2, int arg3) {
+		// TODO Auto-generated method stub
+		
+		
+	}
 
 }
